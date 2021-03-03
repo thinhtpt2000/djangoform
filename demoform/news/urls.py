b875f285083408ba7325ff1e65b8faef753cd88c
thinhtpt2000 <thinhtpt.2000@gmail.com>
2021-03-03 23:13:44 +0700

    add django form

--- demoform/news/urls.py
+++ demoform/news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
 
+app_name = "news"
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('add/', views.add_post, name='add'),
+    path('save/', views.save_post, name='save'),
 ]
--- demoform/news/views.py
+++ demoform/news/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import PostForm
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello everyone!")
+
+def add_post(request):
+    f = PostForm()
+    return render(request, 'news/add_post.html', { "f": f })
+
+def save_post(request):
+    if request.method == "POST":
+        data = PostForm(request.POST)
+        if data.is_valid():
+            data.save()
+            return HttpResponse('Save successfully')
+        else:
+            return HttpResponse('Invalid form')
+    else:
+        return HttpResponse('Invalid post request')
