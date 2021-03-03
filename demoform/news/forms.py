0d1e5f6dacd29d3d6e235a12bd033b0e51ea775b
Thinh Tran <38158427+thinhtpt2000@users.noreply.github.com>
2021-03-03 23:17:20 +0700

    Revert "[Bai10][ThinhTPT] Init common django form"

--- demoform/news/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post
-
-# Create your forms here.
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'content', 'time_create',)
